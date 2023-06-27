26aef44a631686995ba1460bd5467836e65317a7
Tales <jobbtales@gmail.com>
2023-06-27 19:38:29 -0300

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Content from "./components/Content";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <Hero />
+      <Content />
     </>
   )
 }

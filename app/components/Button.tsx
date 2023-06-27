975e31d2d92e808453c8b44a998aeb6e07ccde70
Tales <jobbtales@gmail.com>
2023-06-27 19:35:16 -0300

    feat: new button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+interface ButtonProps {
+    label: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    isActive?: boolean;
+    disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    onClick,
+    isActive,
+    disabled
+}) => {
+  return (
+    <button
+        onClick={onClick}
+        className={`
+            disabled:opacity-70
+            disabled:cursor-not-allowed
+            rounded-full
+            border
+            transition
+            px-4
+            py-2
+            text-[12px]
+            font-semibold
+            hover:border-blueprimary
+            ${!isActive && 'hover:text-blueprimary'}
+            ${isActive ? 'bg-blueprimary' : 'bg-white'}
+            ${isActive ? 'text-white' : 'text-black'}
+        `}
+    >
+        {label}
+    </button>
+  )
+}
+
+export default Button

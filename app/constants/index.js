1e4eb81e7cc4d1c5046256eb3de5c87e58de4151
Tales <jobbtales@gmail.com>
2023-06-27 19:35:52 -0300

    added folder with constants data

---
+++ app/constants/index.js
@@ -0,0 +1,50 @@
+export const contentLinks = [
+    {
+        id: "webinar1",
+        title: "Como aumentar sua Geração de Leads feat. Traktor",
+        type: "Webinar",
+        desc: "Lorem ipsum dolor sit amet",
+        src: "",
+        thumb: ""
+    },
+    {
+        id: "webinar2",
+        title: "Como aumentar sua Geração de Leads feat. Traktor",
+        type: "Webinar",
+        desc: "Lorem ipsum dolor sit amet",
+        src: "",
+        thumb: ""
+    },
+    {
+        id: "webinar3",
+        title: "Como aumentar sua Geração de Leads feat. Traktor",
+        type: "Webinar",
+        desc: "Lorem ipsum dolor sit amet",
+        src: "",
+        thumb: ""
+    },
+    {
+        id: "webinar4",
+        title: "Como aumentar sua Geração de Leads feat. Traktor",
+        type: "Webinar",
+        desc: "Lorem ipsum dolor sit amet",
+        src: "",
+        thumb: ""
+    },
+]
+
+export const optionsSelect = [
+    { value: "Teste1", label: "Data de Publicação" },
+    { value: "Teste2", label: "Teste2" },
+    { value: "Teste3", label: "Teste3" },
+    { value: "Teste4", label: "Teste4" },
+    { value: "Teste5", label: "Teste5" }
+]
+
+export const buttonOptionsNav = [
+    { value: "Agências", label: "Agências" },
+    { value: "Chatbot", label: "Chatbot" },
+    { value: "Marketing Digital", label: "Marketing Digital" },
+    { value: "Geração de Leads", label: "Geração de Leads" },
+    { value: "Mídia Paga", label: "Mídia Paga" },
+]

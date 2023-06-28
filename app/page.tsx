e2a0861bffb991b222c70461473475c2999964d5
Tales <jobbtales@gmail.com>
2023-06-28 16:39:34 -0300

    added HeroFooter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Content from "./components/Content";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import HeroFooter from "./components/HeroFooter";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Content />
+      <HeroFooter />
     </>
   )
 }

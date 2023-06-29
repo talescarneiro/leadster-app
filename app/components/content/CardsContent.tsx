7da98e42f83c546a1aaca5c5900e7832f0748c79
Tales <jobbtales@gmail.com>
2023-06-29 16:03:16 -0300

    added modal function to the card

--- app/components/content/CardsContent.tsx
+++ app/components/content/CardsContent.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import { useState } from "react";
+import Modal from "../Modal";
 
 interface Card {
     id: string;
@@ -14,20 +16,36 @@ interface CardsContentProps {
 }
 
 const CardsContent: React.FC<CardsContentProps> = ({ currentCards }) => {
+    const [modalOpen, setModalOpen] = useState(false)
+    const [selectedCard, setSelectedCard] = useState<Card | null>(null);
+
+    const openModal = (card: Card) => {
+        setSelectedCard(card)
+        setModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setModalOpen(false)
+        setSelectedCard(null)
+    }
+
+
     return (
         <div className='flex flex-col items-center justify-center'>
             <div className='flex flex-wrap justify-between gap-y-6'>
-                {currentCards.map((card: any) => (
-                    <div key={card.title} className='flex flex-col shadow-lg rounded-xl w-[300px]'>
-                        <div className='object-cover'>
+                {currentCards.map((card: Card) => (
+                    <div key={card.title} onClick={() => openModal(card)} className='flex flex-col shadow-lg rounded-xl w-[300px] cursor-pointer group'>
+                        <div className='object-cover relative'>
                             <Image className='object-cover' src={card.thumb} alt='Card Thumbnail' width="300" height="300" />
+                            <div className='absolute inset-0 bg-blue-400 opacity-0 group-hover:opacity-50 transition-opacity'></div>
                         </div>
                         <div className='p-5'>
-                            <h2 className='font-semibold'>{card.title}</h2>
+                            <h2 className='font-semibold transition text-black group-hover:text-blue-500'>{card.title}</h2>
                         </div>
                     </div>
                 ))}
             </div>
+            {modalOpen && selectedCard && (<Modal card={selectedCard} closeModal={closeModal} />)}
         </div>
     )
 }

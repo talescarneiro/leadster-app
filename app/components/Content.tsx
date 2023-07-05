538180afafe050eda399dad7609f4cb229ca303e
Tales <jobbtales@gmail.com>
2023-07-05 17:28:28 -0300

    fix: fix all the category cards function and paginate cards

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,102 +1,89 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import { contentCard } from '../constants';
 import CategoryContent from './content/CategoryContent';
 import CardsContent, { Card } from './content/CardsContent';
 import PageContent from './content/PageContent';
-import { categoryOptions, optionsSelect } from "@/app/constants"
 
-const Content = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [cardsPerPage, setCardsPerPage] = useState(9);
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+import { optionsSelect } from "@/app/constants"
+
+interface ContentProps {
+  currentCards: Card[];
+  setCurrentCards: Dispatch<SetStateAction<Card[]>>;
+}
+
+const Content: React.FC<ContentProps> = ({ currentCards, setCurrentCards }) => {
   const [selectedSortOption, setSelectedSortOption] = useState<string>(optionsSelect[0].value);
-  const [isActiveButtonCategory, setIsActiveButtonCategory] = useState<number | null>(null)
+  const [isActiveButtonCategory, setIsActiveButtonCategory] = useState<number | null>(null);
 
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
   const [selectedCard, setSelectedCard] = useState<Card | null>(null);
 
-  const indexOfLastCard = currentPage * cardsPerPage;
-  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-  const filteredCards = selectedCategory ? contentCard.filter((card) => card.category === selectedCategory) : contentCard;
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [cardsPerPage, setCardsPerPage] = useState<number>(9)
 
-  const totalFilteredCards = filteredCards.length;
-  const totalPages = Math.ceil(totalFilteredCards / cardsPerPage);
-
-  //
-  let sortedCards = [...filteredCards];
-
-  if (selectedSortOption === 'Data de Publicação') {
-    sortedCards.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-  } else if (selectedSortOption === 'Ordem Alfabética') {
-    sortedCards.sort((a, b) => a.title.localeCompare(b.title));
-  }
-
-  const currentCards = sortedCards.slice(indexOfFirstCard, Math.min(indexOfLastCard, sortedCards.length));
-
-  const handleSortOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedSortOption(event.target.value);
-  };
+  const pages = Math.ceil(currentCards.length / cardsPerPage)
+  const lastIndex = currentPage * cardsPerPage
+  const firstIndex = lastIndex - cardsPerPage
+  const currentCardsPage = currentCards.slice(firstIndex, lastIndex)
 
   const openModal = (card: Card) => {
-    setSelectedCard(card)
-    setModalOpen(true)
-  }
+    setSelectedCard(card);
+    setModalOpen(true);
+  };
 
   const closeModal = () => {
-      setModalOpen(false)
-      setSelectedCard(null)
+    setModalOpen(false);
+    setSelectedCard(null);
+  };
+
+  const handleButtonCategoryClick = (index: number, category: string) => {
+    if (isActiveButtonCategory === index) {
+      setIsActiveButtonCategory(null);
+      setCurrentPage(1)
+      setCurrentCards(contentCard);
+    } else {
+      setIsActiveButtonCategory(index);
+      setCurrentPage(1)
+      setCurrentCards(() => {
+        const filteredCardCategory = contentCard.filter((card: Card) => card.category === category);
+        console.log(currentCards)
+        return filteredCardCategory;
+      });
+    }
+  };
+
+  const handleSortOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSortOption(event.target.value)
+    setCurrentPage(1)
+    if (event.target.value === 'Ordem Alfabética') {
+      setCurrentCards((prevCards: Card[]) => {
+        const sortedCards = [...prevCards].sort((a: Card, b: Card) => a.title.localeCompare(b.title))
+        return sortedCards
+      })
+    }
+
+    if (event.target.value === 'Data de Publicação') {
+      setCurrentCards((prevCards: Card[]) => {
+        const sortedCards = [...prevCards].sort((a: Card, b: Card) => new Date(b.date).getTime() - new Date(a.date).getTime())
+        return sortedCards
+      })
+    }
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
   }
-
-  const handleButtonCategoryClick = (index: number) => {
-      if (isActiveButtonCategory === index) {
-          setIsActiveButtonCategory(null);
-          handleSelectCategory(null)
-          setCurrentPage(1)
-      } else {
-          setIsActiveButtonCategory(index);
-          handleSelectCategory(categoryOptions[index].value)
-          setCurrentPage(1)
-      }
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 956) {
-        setCardsPerPage(3);
-      } else {
-        setCardsPerPage(9);
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const handleClickPage = (page: number) => {
-    setCurrentPage(page);
-    window.scrollTo({ top: 500, behavior: 'smooth' });
-  };
-
-  const handleSelectCategory = (category: string | null) => {
-    setSelectedCategory(category);
-    setCurrentPage(1);
-  };
 
   return (
     <main className='flex flex-col justify-center my-9 items-center'>
       <div className='w-65-screen flex flex-col justify-center items-center'>
         <CategoryContent handleButtonCategoryClick={handleButtonCategoryClick} isActiveButtonCategory={isActiveButtonCategory} selectedSortOption={selectedSortOption} handleSortOptionChange={handleSortOptionChange} />
         <div className='border m-3 w-full' />
-        <CardsContent currentCards={currentCards} openModal={openModal} closeModal={closeModal} modalOpen={modalOpen} selectedCard={selectedCard} />
+        <CardsContent currentCards={currentCardsPage} openModal={openModal} closeModal={closeModal} modalOpen={modalOpen} selectedCard={selectedCard} />
         <div className='border m-3 w-full' />
-        <PageContent totalPages={totalPages} currentPage={currentPage} handleClickPage={handleClickPage} />
+        <PageContent pages={pages} currentPage={currentPage} setCurrentPage={handlePageChange} />
       </div>
     </main>
   );

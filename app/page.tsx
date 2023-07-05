21573c193534cd8c34b3facd76358446a19e4838
Tales <jobbtales@gmail.com>
2023-07-05 17:31:53 -0300

    feat: contentCards state

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
+import { useState } from "react";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import HeroFooter from "./components/HeroFooter";
+import { Card } from "./components/content/CardsContent";
+import { contentCard } from "./constants";
 
 export default function Home() {
+  const [currentCards, setCurrentCards] = useState<Card[]>(contentCard || [])
+
   return (
     <>
       <Header />
       <Hero />
-      <Content/>
+      <Content currentCards={currentCards} setCurrentCards={setCurrentCards}/>
       <HeroFooter />
       <Footer />
     </>

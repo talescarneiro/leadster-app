4e1d515422b27c7ddd528da661ba88cc85bdf732
Tales <jobbtales@gmail.com>
2023-07-05 17:29:51 -0300

    fix: paginate function

--- app/components/content/PageContent.tsx
+++ app/components/content/PageContent.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 
-type ClickPageHandler = (page: number) => void;
-
 interface PageContentProps {
-  totalPages: number;
+  pages: number;
   currentPage: number;
-  handleClickPage: ClickPageHandler;
+  setCurrentPage: (pageNumber: number) => void;
 }
 
-const PageContent: React.FC<PageContentProps> = ({ totalPages, currentPage, handleClickPage }) => {
+const PageContent: React.FC<PageContentProps> = ( { pages, currentPage, setCurrentPage }) => {
+  const handlePageClick = (page: number) => {
+    setCurrentPage(page)
+    window.scrollTo({ top: 500, behavior: 'smooth' })
+  }
+
   return (
     <div className='flex gap-3 items-center'>
       <h3 className='font-semibold'>Página</h3>
-      {totalPages > 0 &&
-        Array.from({ length: totalPages }).map((_, index) => (
-          <button
-            key={index + 1}
-            className={`px-3 py-1 rounded-md border ${
-              currentPage === index + 1 ? 'border-blue-500 text-blue-500 font-semibold' : 'border-none'
-            }`}
-            onClick={() => handleClickPage(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
+      {Array.from(Array(pages), (item, index) => {
+        return (
+          <button className={`px-3 py-1 rounded-md border ${currentPage === index + 1 ? 'border-blue-500 text-blue-500 font-semibold' : 'border-none'}`} key={index} value={index + 1} onClick={() => handlePageClick(index + 1)}>{index + 1}</button>
+        )
+      })}
     </div>
   );
 };

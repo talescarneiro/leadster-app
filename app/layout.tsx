629c52eba5ff173721224a816a0364700bc1f6c9
Tales <jobbtales@gmail.com>
2023-07-05 21:09:18 -0300

    added jakarta sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="plus-jakarta-sans">{children}</body>
     </html>
   )
 }

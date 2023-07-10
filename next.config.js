793384b1f8cd10e2f98673bfd4196d0dff03aaab
Tales <jobbtales@gmail.com>
2023-07-10 11:44:32 -0300

    added icon to manifest

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  manifest: {
+    icons: {
+      src: '/assets/images/logo.png', 
+      cache: true,
+      sizes: [192, 512], 
+      type: 'image/png', 
+    },
+  },
+};
